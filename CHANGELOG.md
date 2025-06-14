c591fc654cbed5dfcfad61b120e3972f03b030d5
Andrew Zhong <36863876+azhong-git@users.noreply.github.com>
2025-06-14 12:47:22 -0700

    Use playwright core on serverless env (#11)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Fixed
+- Used playwright-core in serverless environments
+
 ## [0.1.0] - 2025-06-13
 
 ### Added
--- src/types/browser-providers/types.ts
+++ src/types/browser-providers/types.ts
@@ -4,7 +4,7 @@ abstract class BrowserProvider<T> {
   abstract session: unknown;
   abstract start(): Promise<Browser>;
   abstract close(): Promise<void>;
-  abstract getSession(): T|null;
+  abstract getSession(): T | null;
 }
 
 export default BrowserProvider;
